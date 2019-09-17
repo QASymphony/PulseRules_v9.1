9b41b944d4e3d6a7f696a46c8aaecc72331baef1
Nhat Tran <nhattd97@gmail.com>
2019-09-17 22:36:28 +0700

    Refactor LinkRequirement.js

--- LinkRequirement.js
+++ LinkRequirement.js
@@ -1,14 +1,22 @@
+/**
+ * payload example:
+ * {
+     {number} tcid - Your Qtest test case id
+     {number} issueKey - Your Jira issue id
+ * }
+ * Constant:
+ * - ManagerURL: Your qtest url (i.e techsupport.qtestnet.com)
+ * - QTEST_TOKEN: Your qtest token (i.e 1038cf25-4e14-4332-bcb0-7444cd747905)
+ * - ProjectID: Your qtest project id (i.e 229509)
+ * outputs:
+ * - Link an existing qTest Test Case to a Jira requirement
+ * Note:
+ * - Jira & qTest integration is already set up and that Jira Issue already exists in qTest as a requirement
+ */
+
 const { Webhooks } = require('@qasymphony/pulse-sdk');
 const request = require('request');
-const xml2js = require('xml2js');
 
-//
-// Expects the payload to look like this
-// {
-//   tcid: 12345,
-//   issueKey: 'AI-123'
-// }
-//
 exports.handler = function ({ event: body, constants, triggers }, context, callback) {
   function emitEvent(name, payload) {
       let t = triggers.find(t => t.name === name);
@@ -70,5 +78,5 @@ exports.handler = function ({ event: body, constants, triggers }, context, callb
         ]
     };
   }
-}
+};
 
